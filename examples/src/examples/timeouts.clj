0d9dfd0da2112afe492f36bd7b303fe92c349851
ronen <narkisr@gmail.com>
2014-11-25 00:20:16 +0200

    latest examples slides

--- examples/src/examples/timeouts.clj
+++ examples/src/examples/timeouts.clj
@@ -7,6 +7,12 @@
 
 (<!! (timeout 1000))
 
+; [nil, timeout-channel]
 (alts!! [a (timeout 1000)])
 
+; put into a -> no takers so [nil, timeout-channel]
+(alts!! [[a 42] (timeout 1000)])
+
+; [:meh :default]
+(alts!! [a] :default :meh) 
 
